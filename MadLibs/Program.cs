72dff715a5fdb591b3b7452a9b8dc1ea48f15511
gcarpenter7@my.bcit.ca <gcarpenter7@my.bcit.ca>
2020-04-13 23:31:35 -0700

    some methods for common choices added

--- MadLibs/Program.cs
+++ MadLibs/Program.cs
@@ -6,24 +6,83 @@ namespace MadLibs
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Welcome to Mad Libs");
-            Console.WriteLine("-------------------");
-            Console.WriteLine("Have you played Mad Libs before? Y / N");
-            string response = Console.ReadLine();
-            if(response == "Y" || response == "y")
+            GoToMainMenu();
+
+            void GoToMainMenu()
             {
-                Console.WriteLine("Okay then! Let's start then!");
-                Console.WriteLine("Pick a category:");
-                Console.WriteLine("1. Romance");
-                Console.WriteLine("2. Action");
-                Console.ReadKey();
-            } else if (response == "N" || response == "n")
+                Console.WriteLine("Welcome to Mad Libs");
+                Console.WriteLine("-------------------");
+                Console.WriteLine("Have you played Mad Libs before? Y / N");
+
+                string response = Console.ReadLine();
+                if (response == "Y" || response == "y")
+                {
+                    PickCategory();
+                }
+                else if (response == "N" || response == "n")
+                {
+                    Console.WriteLine("No problem. Here's how you play: " + Definitions.instructions);
+                    Console.WriteLine();
+                    PickCategory();
+                }
+                else
+                {
+                    Console.WriteLine("Please select either Y for yes or N for no");
+                    GoToMainMenu();
+                }
+            }
+
+            void PickCategory()
             {
-                Console.WriteLine("No problem. Here's how you play: " + Definitions.instructions);
-                Console.ReadLine();
-            } else
+                Console.WriteLine("Alright, let's play!");
+                Console.WriteLine("Pick a story:");
+                Console.WriteLine("1. Row Row Row Your ______");
+                Console.WriteLine("2. I wanna know what ______ is.");
+                int storyChoice = Convert.ToInt32(Console.ReadLine());
+                if (storyChoice == 1)
+                {
+                    PlayRowYourBoat();
+                }
+                else if (storyChoice == 2)
+                {
+                    PlayForeigner();
+                }
+                else
+                {
+                    Console.WriteLine("Sorry that choice is invalid. Please choose 1 or 2");
+                    PickCategory();
+                }
+            }
+
+            void PlayRowYourBoat()
             {
-                Console.WriteLine("Please select either Y for yes or N for no");
+                RowStory myRowStory = new RowStory();
+                Console.WriteLine("You picked Row, row, row your ______");
+                Console.WriteLine("Pick a noun");
+                myRowStory.boatNoun = Console.ReadLine();
+
+                Console.WriteLine("Pick another noun");
+                myRowStory.streamNoun = Console.ReadLine();
+
+                Console.WriteLine("Pick an adverb");
+                myRowStory.merrilyAdverb = Console.ReadLine();
+
+                Console.WriteLine("Pick another adverb");
+                myRowStory.merrily2 = Console.ReadLine();
+
+                Console.WriteLine("Pick a third adverb");
+                myRowStory.merrily3 = Console.ReadLine();
+
+                Console.WriteLine("Pick a noun finally");
+                myRowStory.dreamNoun = Console.ReadLine();
+
+                //ReadRow();
+                Console.WriteLine($"Row, row, row your {myRowStory.boatNoun} gently down the {myRowStory.streamNoun}. {myRowStory.merrilyAdverb}, merrily, {myRowStory.merrily2}, {myRowStory.merrily3} life is but a {myRowStory.dreamNoun}.");
+            }
+
+            void PlayForeigner()
+            {
+                Console.WriteLine("You picked 'I wanna know what  ______ is.");
             }
         }
     }
---
+++ MadLibs/RowStory.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace MadLibs
+{
+    public class RowStory
+    {
+        public string boatNoun = "boat";
+        public string streamNoun = "stream";
+        public string merrilyAdverb = "merrily";
+        public string merrily2 = "merrily";
+        public string merrily3 = "merrily";
+        public string dreamNoun = "noun";
+
+    }
+}
