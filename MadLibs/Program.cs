f1405509da78d622591b520a64edc0238d1f5565
gcarpenter7@my.bcit.ca <gcarpenter7@my.bcit.ca>
2020-05-10 20:41:41 -0700

    Added all instructions to program for motivational story. Issue #1

--- MadLibs/Program.cs
+++ MadLibs/Program.cs
@@ -171,6 +171,9 @@ namespace MadLibs
 
                 Console.WriteLine("Last one! Pick a number");
                 m.number4 = Console.ReadLine();
+
+                Console.WriteLine("Here's the story you created: entitled 'Not just any motivational YouTube Ad'");
+                Console.WriteLine($"Hi, my name is {m.celeb} and I'm gonna teach you about how I make $ {m.number} while I'm sleeping. Pay attention, because I'm going to tell you stuff not even {m.talkhost} will tell you about finance. I'm coming to you from my garage, just took my {m.carBrand} out for a spin. I know what you're thinking. You see me standing in front of my {m.number2} {m.carBrand2}s and you're asking yourself, '{m.celeb} what's your secret?' Pay attention and I'll teach you everything. About six years ago, I was working at {m.nounPlace} and my mentor {m.nounPerson} came in wearing {m.clothingBrand} shoes and I just knew I had to be like him. I took him out for {m.cheapBeverage} and he told me, '{m.celeb}, you need to dropship {m.pluralNoun}.' At that time I had no idea what dropshipping was, but what he told me was so simple. All you have to do is just buy {m.number2} {m.pluralNoun2} from {m.country} and sell them at a {m.percentage} markup on {m.website}. That's it! That is all I do. Then I just cash my cheque for ${m.amount} and then I can spend it on whatever I want. If I want to vacation in {m.city} no one's gonna stop me. I've got the freedom to buy {m.pluralNoun3} every day if I want to. I'll tell you what, {m.percentage2} of people won't even try this. They'll be stuck {m.verbING} all day at a {m.noun} they hate and I'll be laughing, drinking my {m.beverage} by the {m.noun2}. And you can too if you buy my ebook, 'How to {m.verb} {m.adverb}' but only for the next {m.number4} minutes. We can't do this all day.");
             }
         }
     }
