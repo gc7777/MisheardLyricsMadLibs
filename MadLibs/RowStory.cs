dda32c5195a0d73eee5b4ac6ae8595e4d16641a5
gcarpenter7@my.bcit.ca <gcarpenter7@my.bcit.ca>
2020-05-19 14:36:17 -0700

    Align row story; remove excess

--- MadLibs/RowStory.cs
+++ MadLibs/RowStory.cs
@@ -6,12 +6,12 @@ namespace MadLibs
 {
     public class RowStory
     {
-        public string boatNoun = "boat";
-        public string streamNoun = "stream";
-        public string merrilyAdverb = "merrily";
-        public string merrily2 = "merrily";
-        public string merrily3 = "merrily";
-        public string dreamNoun = "noun";
+        public string boatNoun      = "boat",
+                      streamNoun    = "stream",
+                      merrilyAdverb = "merrily",
+                      merrily2      = "merrily",
+                      merrily3      = "merrily",
+                      dreamNoun     = "noun";
 
     }
 }
